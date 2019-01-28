eeefba689b3c9cd44e63d6037bf45c2f989f3a05
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-01-28 16:30:31 -0300

    Limpieza

--- WindowsCuotasApp/FormListadoSociosCuotas.Designer.cs
+++ WindowsCuotasApp/FormListadoSociosCuotas.Designer.cs
@@ -50,9 +50,9 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(20, 49);
+            this.dataGridView1.Location = new System.Drawing.Point(20, 34);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(892, 317);
+            this.dataGridView1.Size = new System.Drawing.Size(892, 332);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
