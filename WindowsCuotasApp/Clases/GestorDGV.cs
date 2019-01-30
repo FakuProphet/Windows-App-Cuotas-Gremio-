7392a786832fb13650a27df33cf42feb2fa44dd9
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-01-30 15:46:37 -0300

    Creando clase GestorDGV, que maneja apariencia y funcionalidad de los objetos del tipo DataGridView

---
+++ WindowsCuotasApp/Clases/GestorDGV.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsCuotasApp.Clases
+{
+    class GestorDGV
+    {
+
+
+        public void efectosDGV(DataGridView miDGV)
+        {
+            miDGV.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise;
+            miDGV.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            miDGV.BackgroundColor = Color.White;
+            miDGV.EnableHeadersVisualStyles = false;
+            miDGV.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            miDGV.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            miDGV.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+        }
+
+
+        public void cargarDataGrid(DataGridView miDataGrid, string consulta)
+        {
+            DataSet miDataSet = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(consulta, Conectar.ObtenerConexion());
+            da.Fill(miDataSet);
+            Conectar.CerrarConexion();
+            miDataGrid.RowHeadersVisible = false;
+            miDataGrid.AllowUserToAddRows = false;
+            miDataGrid.AllowUserToDeleteRows = false;
+            miDataGrid.AllowUserToOrderColumns = false;
+            miDataGrid.AllowUserToResizeColumns = true;
+            miDataGrid.AllowUserToResizeRows = false;
+            miDataGrid.AutoResizeColumns();
+            miDataGrid.DataSource = miDataSet.Tables[0];
+            //estas dos lineas siguientes indican q las celdas se ajusten al contenido
+            //-----------------------------------------------------------------------
+            miDataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            miDataGrid.BorderStyle = BorderStyle.None;
+            //---------------------------------------------------------------------
+        }
+    }
+}
