5dcaae42b2f7491709d6e7a762c491330266329f
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-01-30 15:52:17 -0300

    Codificando en evento load formulario listado de socios con débito.

--- WindowsCuotasApp/FormListadoSociosCuotas.cs
+++ WindowsCuotasApp/FormListadoSociosCuotas.cs
@@ -1,18 +1,22 @@
 ﻿using System;
-
+using WindowsCuotasApp.Clases;
 
 namespace WindowsCuotasApp
 {
     public partial class FormListadoSociosCuotas : MetroFramework.Forms.MetroForm
     {
+        GestorDGV gestorDGV;
+
         public FormListadoSociosCuotas()
         {
             InitializeComponent();
+            gestorDGV = new GestorDGV();
         }
 
         private void FormListadoSociosCuotas_Load(object sender, EventArgs e)
         {
-
+            gestorDGV.efectosDGV(dataGridView1);
+            gestorDGV.cargarDataGrid(dataGridView1, "SELECT * FROM V_LISTADO_SOCIOS_PAGO_DEBITO ORDER BY 2 ASC");
         }
     }
 }
