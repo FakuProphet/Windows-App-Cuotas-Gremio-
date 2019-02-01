46044ff0661ba09008f3ba3ea324f573cf9b3b5c
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-02-01 18:55:12 -0300

    Avances reporte listado de afiliados a debitar cuota

--- WindowsCuotasApp/FormReporte.Designer.cs
+++ WindowsCuotasApp/FormReporte.Designer.cs
@@ -28,23 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.ReporteListadoAfiliadosDebito1 = new WindowsCuotasApp.ReporteListadoAfiliadosDebito();
             this.SuspendLayout();
             // 
-            // reportViewer1
+            // crystalReportViewer1
             // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.reportViewer1.Location = new System.Drawing.Point(20, 60);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(760, 370);
-            this.reportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(20, 60);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.ReporteListadoAfiliadosDebito1;
+            this.crystalReportViewer1.ShowLogo = false;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(760, 370);
+            this.crystalReportViewer1.TabIndex = 0;
             // 
             // FormReporte
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.crystalReportViewer1);
             this.Name = "FormReporte";
             this.Text = "Reporte Listado afiliados a debitar";
             this.Load += new System.EventHandler(this.FormReporte_Load);
@@ -52,7 +58,8 @@
 
         }
 
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private ReporteListadoAfiliadosDebito ReporteListadoAfiliadosDebito1;
         //  private CrystalReport1 CrystalReport11;
 
         //private CrystalReport1 CrystalReport11;
