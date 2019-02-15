276d975ce7932458cc668845b16bb530e3cb4bcf
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-02-15 16:55:09 -0300

    Loguin Usuario

---
+++ WindowsCuotasApp/Clases/LOGUIN/GestorLoguin.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsCuotasApp.Clases.LOGUIN
+{
+    class GestorLoguin
+    {
+
+        SqlDataReader dr;
+
+        public SqlDataReader Dr { get => dr; set => dr = value; }
+
+        public User GetAfiliadoPorDni(string user,string pass)
+        {
+
+            User u = null;
+
+            SqlCommand cmd = new SqlCommand("SP_GET_USUARIO_APP", Conectar.ObtenerConexion());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@USUARIO", SqlDbType.VarChar).Value = user;
+            cmd.Parameters.Add("@CONTRASENIA", SqlDbType.VarChar).Value = user;
+
+            Dr = cmd.ExecuteReader();
+            Conectar.CerrarConexion();
+            if (Dr.Read())
+            {
+
+                string usuario = Dr.GetString(0);
+                string contrasenia = Dr.GetString(1);
+
+                u = new User { Usuario = usuario, Constraseña = contrasenia };
+
+            }
+
+            return u;
+        }
+    }
+}
---
+++ WindowsCuotasApp/Clases/LOGUIN/User.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace WindowsCuotasApp.Clases.LOGUIN
+{
+    class User
+    {
+        public string Usuario { get; set; }
+        public string Constraseña { get; set; }
+    }
+}
