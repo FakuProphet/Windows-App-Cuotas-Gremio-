ee3d829da6010a5d5d46f3648c7e5a118e54757c
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-07-18 19:40:41 -0300

    Avances en ventana de invitados

--- WindowsCuotasApp/FormInvitados.Designer.cs
+++ WindowsCuotasApp/FormInvitados.Designer.cs
@@ -32,7 +32,14 @@
             this.cboMesesHabilitados = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMesActual = new System.Windows.Forms.Label();
+            this.btnFiltro = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFiltroDni = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // cboMesesHabilitados
@@ -40,6 +47,7 @@
             this.cboMesesHabilitados.FormattingEnabled = true;
             resources.ApplyResources(this.cboMesesHabilitados, "cboMesesHabilitados");
             this.cboMesesHabilitados.Name = "cboMesesHabilitados";
+            this.cboMesesHabilitados.SelectedIndexChanged += new System.EventHandler(this.cboMesesHabilitados_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -52,18 +60,61 @@
             resources.ApplyResources(this.dataGridView1, "dataGridView1");
             this.dataGridView1.Name = "dataGridView1";
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView2, "dataGridView2");
+            this.dataGridView2.Name = "dataGridView2";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // lblMesActual
+            // 
+            resources.ApplyResources(this.lblMesActual, "lblMesActual");
+            this.lblMesActual.ForeColor = System.Drawing.Color.Blue;
+            this.lblMesActual.Name = "lblMesActual";
+            // 
+            // btnFiltro
+            // 
+            resources.ApplyResources(this.btnFiltro, "btnFiltro");
+            this.btnFiltro.Name = "btnFiltro";
+            this.btnFiltro.UseVisualStyleBackColor = true;
+            this.btnFiltro.Click += new System.EventHandler(this.btnFiltro_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txtFiltroDni
+            // 
+            resources.ApplyResources(this.txtFiltroDni, "txtFiltroDni");
+            this.txtFiltroDni.Name = "txtFiltroDni";
+            this.txtFiltroDni.TextChanged += new System.EventHandler(this.txtFiltroDni_TextChanged);
+            // 
             // FormInvitados
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtFiltroDni);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnFiltro);
+            this.Controls.Add(this.lblMesActual);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cboMesesHabilitados);
             this.MaximizeBox = false;
             this.Name = "FormInvitados";
             this.Opacity = 0.9D;
+            this.Resizable = false;
             this.Load += new System.EventHandler(this.FormInvitados_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,5 +125,11 @@
         private System.Windows.Forms.ComboBox cboMesesHabilitados;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMesActual;
+        private System.Windows.Forms.Button btnFiltro;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFiltroDni;
     }
 }
--- WindowsCuotasApp/FormInvitados.cs
+++ WindowsCuotasApp/FormInvitados.cs
@@ -15,6 +15,8 @@ namespace WindowsCuotasApp
     {
         GestorDGV g;
         GestorAfiliados ga;
+        private DataTable dt = new DataTable();
+        private DataSet ds = new DataSet();
         public FormInvitados()
         {
             InitializeComponent();
@@ -26,8 +28,59 @@ namespace WindowsCuotasApp
         {
             g.efectosDGV(dataGridView1);
             ga.CargarCombo(cboMesesHabilitados);
+            MiMes();
+            ValidarCierreFacturacion();
+
         }
 
-        
+        private void cboMesesHabilitados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string miMes = cboMesesHabilitados.SelectedText;
+            g.cargarDataGrid(dataGridView1, consulta: "select * from v_invitado_uno where Mes like '" + miMes + "' ");
+            g.efectosDGV(dataGridView1);
+        }
+
+        private void ValidarCierreFacturacion()
+        {
+            DateTime fecha = DateTime.Now;
+            int diaDelMes = fecha.Day;
+
+            if (diaDelMes > 17)
+            {
+                btnFiltro.Enabled = true;
+            }
+        }
+
+
+        private void MiMes()
+        {
+            DateTime hoy = DateTime.Now;
+            string mes = hoy.ToString("MMMM"); //te da el nombre completo en la cultura default
+            lblMesActual.Text = mes.ToUpper();
+            // string mes2 = hoy.ToString("MMMM", CultureInfo.CreateSpecificCulture("en-US")); //en ingles
+        }
+
+        private void btnFiltro_Click(object sender, EventArgs e)
+        {
+            string miMes = lblMesActual.Text.Trim();
+            g.cargarDataGrid(dataGridView2, consulta: "select * from v_invitado_uno where Mes like '" + miMes + "' ");
+            g.efectosDGV(dataGridView2);
+        }
+
+        private void txtFiltroDni_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                (dataGridView2.DataSource as DataTable).DefaultView.RowFilter =
+                string.Format("DNI LIKE '{0}%' OR DNI LIKE '% {0}%'", txtFiltroDni.Text);
+
+            }
+            catch (Exception)
+            {
+
+               
+            }
+
+        }
     }
 }
